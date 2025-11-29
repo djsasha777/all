23c182b8e54d4f1df169c56b0833abe7ea0b370d
Alex <djsasha777@mail.ru>
2025-11-29 15:35:44 +0300

    add

---
+++ iac/openwrt/xiaomi-r3g/scripts/nginx-allconf.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+git clone https://git.spongo.ru/all.git
+cp -rf all/iac/openwrt/bpi-r3-mini/nginx/* /etc/nginx/
+mv /etc/nginx/nginx-allconf.sh /root/run.sh
+rm -rf hardware
+chmod +x /root/run.sh
+chmod 644 -R /etc/nginx
+chmod 755 /etc/nginx
+chmod 755 /etc/nginx/player
+service nginx restart
+echo "files copied and nginx restarted."
