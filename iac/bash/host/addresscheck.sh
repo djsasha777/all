31135a7497a2f0211750857c211e2d7a96caea76
Alex <djsasha777@mail.ru>
2025-11-29 19:27:17 +0300

    up

--- iac/bash/host/addresscheck.sh
+++
@@ -1,8 +0,0 @@
-# Script for checking global ip address and region
-#!/bin/bash
-apt update
-apt install jq -y
-ip=$(curl -s https://api.ipify.org)
-echo "IP: $ip"
-reg=$(curl -s http://ip-api.com/json/${ip} | jq -r '.countryCode')
-echo "REGION: $reg"
