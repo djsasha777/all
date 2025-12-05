abb4c8ec95685de0287b60c4772f99638d8a6575
Alex <djsasha777@mail.ru>
2025-12-05 08:04:30 +0300

    add

--- iac/openwrt/bpi-r3-mini/scripts/nginx-allconf.sh
+++ iac/openwrt/bpi-r3-mini/scripts/nginx-allconf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-git clone https://git.spongo.ru/all.git
+git clone https://github.com/djsasha777/all.git
 cp -rf all/iac/openwrt/bpi-r3-mini/nginx/* /etc/nginx/
 mv /etc/nginx/nginx-allconf.sh /root/run.sh
-rm -rf hardware
+rm -rf all
 chmod +x /root/run.sh
 chmod 644 -R /etc/nginx
 chmod 755 /etc/nginx
